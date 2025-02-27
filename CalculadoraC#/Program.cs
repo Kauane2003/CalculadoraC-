c18eddd9a1da117e17be66a14262893a26625e1b
KAUANE SOUZA CUNHA <kauane.scunha1@senacsp.edu.br>
2025-02-27 14:40:15 -0300

    Projeto inicial Calculadora Csharp

---
+++ CalculadoraC#/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CalculadoraC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
