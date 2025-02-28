245945b93ab81e35fa51f0deec54b046e5e19ef8
KAUANE SOUZA CUNHA <kauane.scunha1@senacsp.edu.br>
2025-02-28 14:39:09 -0300

    Pedindo numeros que deseja calcular

--- CalculadoraC#/Program.cs
+++ CalculadoraC#/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.Serialization;
-
+﻿
 namespace CalculadoraC_
 {
     internal class Program
@@ -8,13 +7,20 @@ namespace CalculadoraC_
         {
             Console.WriteLine("Qual operação deseja fazer: ");
             Console.WriteLine("1- Adição");
-            Console.WriteLine("2- Subtração ")
+            Console.WriteLine("2- Subtração ");
 
-            int operação = int.Parse(Console.ReadLine();
+            int operacao = int.Parse(Console.ReadLine();
 
-           
+            Console.WriteLine("Digite o primeiro numero: ");
+            int Numero1 = int.Parse(Console.ReadLine();
 
+            Console.WriteLine("Digite o Segundo numero: ");
+            int Numero2 = int.Parse(Console.ReadLine();
 
-        }
+            int resultado = 0;
+
+            
+      
+        
     }
 }
